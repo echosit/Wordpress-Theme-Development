453e9c24a1c529db7c066b34fdf7613bebaf03fa
Echo Sit <echo.sit@comcast.net>
2021-03-25 12:38:13 -0700

    create comment section

--- single.php
+++ single.php
@@ -19,7 +19,30 @@
           </div>
 
         <div id="comments-section">
-          <?php comment_form(); 
+          <?php 
+          
+          $fields = array(
+            'author' =>
+                '<input placeholder="Name" id="author" type="text" value="' .
+                esc_attr( $commenter['comment_author']) .
+                '" size="30"' . $aria_req . ' />',
+
+            'email' =>
+                '<input placeholder="Email" id="email" name="email" type="text" value="' .
+                esc_attr( $commenter['comment_author_email']) .
+                '" size="30"' . $aria_req . ' />',
+          );
+
+          $args = array(
+            'title_reply' => 'Share Your Thoughts',
+            'fields' => $fields,
+            'comment_field' => '<textarea placeholder="Your Comment" id="comment"
+            name="comment" cols="45" rows="8" aria-required="true">' .
+            '</textarea>',
+            'comment_notes_before' => '<p class="comment-notes">Your email address will not be published. All fields are required.</p>'
+          );
+    
+          comment_form($args); 
 
           $comments_number = get_comments_number();
           if($comments_number != 0) {
